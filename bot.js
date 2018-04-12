f470287039bfa8167ae477aecf2c4c43e17c5570
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-04-12 21:42:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
- 
  client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-        console.log(`----------------`);
-      console.log(`Your Bot in  ${client.guilds.size} Servers `);
-              console.log(`----------------`);
-      console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
-              console.log(`----------------`);
-  console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
-    console.log(`----------------`);
-
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
 
