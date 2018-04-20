70b6a71e2d5a33247b97babd45c056b223ed918a
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-04-20 02:52:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,40 +11,6 @@ client.on('message', msg => {
   }
 });
 
-var prefix = "!";
-
-client.on('message', message => {
-if (!points[message.author.id]) points[message.author.id] = {
-	points: 0,
-  };
-if (message.content.startsWith(prefix + 'عواصم')) {
-	if(!message.channel.guild) return
-
-const type = require('./fkk/3wasm.json');
-const item = type[Math.floor(Math.random() * type.length)];
-const filter = response => {
-    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-};
-message.channel.send('**لديك 15 ثانية لتجيب**').then(msg => {
-
-			
-msg.channel.send(`${item.type}`).then(() => {
-        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
-        .then((collected) => {
-		message.channel.send(`${collected.first().author} ✅ **مبروك لقد كسبت نقطه
-لمعرفة نقطاك الرجاء كتابة !نقاطي**`);
-		console.log(`[Typing] ${collected.first().author} typed the word.`);
-			let userData = points[message.author.id];
-			userData.points++;
-          })
-          .catch(collected => {
-            message.channel.send(`:x: **خطأ حاول مرة اخرى**`);
-			console.log('[Typing] Error: No one type the word.');
-          })
-		})
-	})
-}
-});
 
 
    
