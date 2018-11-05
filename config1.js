9f17e2ca064cb3d770267b4a788953f90af21fc0
tawfiqsiam <toto.siam999@gmail.com>
2018-11-05 22:06:55 +0200

    Update config1.js

--- config1.js
+++ config1.js
@@ -1,6 +1,6 @@
 
-exports.TOKEN = 'process.env.BOT_TOKEN'; // NTA4Mzg4NjQ4ODY2MDIxNDAy.DsIzkg.r8VhODEPt7g52hxz4Jpv1gXAEag
+exports.TOKEN = 'NTA4Mzg4NjQ4ODY2MDIxNDAy.DsIzkg.r8VhODEPt7g52hxz4Jpv1gXAEag'; // NTA4Mzg4NjQ4ODY2MDIxNDAy.DsIzkg.r8VhODEPt7g52hxz4Jpv1gXAEag
 
-exports.PREFIX = '1';//برفكس البوت هو يلي بيشغل لاغاني
+exports.PREFIX = 'g';//g
 
 exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8'; // لا تغيره
